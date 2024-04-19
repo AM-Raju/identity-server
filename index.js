926cea0fb191920b52da5c58c26ce3c87e361766
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-04-19 09:22:04 +0600

    Final project initialization

---
+++ index.js
@@ -0,0 +1,172 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("identity");
+    const productsCollection = db.collection("products");
+    const categoriesCollection = db.collection("categories");
+
+    // User Registration
+    app.post("/api/v1/register", async (req, res) => {
+      const { username, email, password } = req.body;
+
+      // Check if email already exists
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exist!!!",
+        });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Insert user into the database
+      await usersCollection.insertOne({
+        username,
+        email,
+        password: hashedPassword,
+        role: "user",
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully!",
+      });
+    });
+
+    // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      // Find user by email
+      const user = await usersCollection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Compare hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign(
+        { email: user.email, role: user.role },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.EXPIRES_IN,
+        }
+      );
+
+      res.json({
+        success: true,
+        message: "User successfully logged in!",
+        accessToken: token,
+      });
+    });
+
+    app.get("/categories", async (req, res) => {
+      const result = await categoriesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/dresses/:category", async (req, res) => {
+      const category = req.params.category;
+
+      const result = await categoriesCollection
+        .aggregate([
+          // Stage one
+
+          {
+            $lookup: {
+              from: "products",
+              localField: "slug",
+              foreignField: "category",
+              as: "dresses",
+            },
+          },
+
+          // Stage two
+          {
+            $match: {
+              slug: category,
+            },
+          },
+        ])
+        .toArray();
+
+      res.send(result);
+    });
+
+    app.get("/all-products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/flash-products", async (req, res) => {
+      const query = { flashSale: true };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const productId = new ObjectId(id);
+      const query = { _id: productId };
+
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/top-reviews", async (req, res) => {
+      const result = await productsCollection
+        .find()
+        .sort({ ratings: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+// Test route
+app.get("/", (req, res) => {
+  const serverStatus = {
+    message: "Server is running smoothly",
+    timestamp: new Date(),
+  };
+  res.json(serverStatus);
+});
