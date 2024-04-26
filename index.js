6b3709e7aae6d874db81304c5780b071e1dad88f
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-04-26 18:15:55 +0600

    Order creation api done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const bcrypt = require("bcrypt");
 const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,13 +26,15 @@ async function run() {
     console.log("Connected to MongoDB");
 
     const db = client.db("identity");
+    const usersCollection = db.collection("users");
     const productsCollection = db.collection("products");
     const categoriesCollection = db.collection("categories");
+    const ordersCollection = db.collection("orders");
 
     // User Registration
     app.post("/api/v1/register", async (req, res) => {
       const { username, email, password } = req.body;
-
+      console.log(username, email, password);
       // Check if email already exists
       const existingUser = await usersCollection.findOne({ email });
       if (existingUser) {
@@ -90,6 +93,23 @@ async function run() {
       });
     });
 
+    /* Auth code ends */
+
+    /* ======= User Block ========== */
+    // Get single user
+
+    app.get("/api/v1/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      let result;
+      if (email) {
+        result = await usersCollection.findOne(query);
+      }
+      res.send(result);
+    });
+
+    /* ======= category Block ========== */
+
     app.get("/categories", async (req, res) => {
       const result = await categoriesCollection.find().toArray();
       res.send(result);
@@ -123,6 +143,8 @@ async function run() {
       res.send(result);
     });
 
+    /* ======= Dress Block ========== */
+
     app.get("/all-products", async (req, res) => {
       const result = await productsCollection.find().toArray();
       res.send(result);
@@ -151,6 +173,16 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+    /* ======= Order Block ========== */
+    app.post("/create-order", async (req, res) => {
+      const order = req.body;
+      console.log("Order", order);
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+
+    /* ======= Review Block ========== */
 
     // Start the server
     app.listen(port, () => {
